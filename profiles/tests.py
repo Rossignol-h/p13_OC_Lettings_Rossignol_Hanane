0859660d5e00a6060de11c730037bf297091d651
Rossignol-h <ha.rossignol@laposte.net>
2023-01-09 16:15:36 +0100

    test(oc_lettings): add all tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,37 @@
-# from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+# ====================================================== TEST PROFILE INDEX
+
+
+@pytest.mark.django_db
+def test_profiles_index(client):
+    url = reverse('profiles:index')
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert b"<title>Profiles</title>" in response.content
+
+# ====================================================== TEST PROFILE RETRIEVE DETAILS
+
+
+@pytest.mark.django_db
+def test_profile_detail(client):
+    user = User(
+        username="MIB",
+        password="meninblack",
+        first_name="Will",
+        last_name="Smith",
+        email="w_smith@mib.com"
+    )
+    user.save()
+
+    profile = Profile(user=user, favorite_city="New York")
+    profile.save()
+
+    response = client.get(reverse('profiles:profile', args=["MIB"]))
+
+    assert response.status_code == 200
+    assert b"<h1>MIB</h1>" in response.content
