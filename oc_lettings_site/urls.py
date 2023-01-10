a862d6445ce704a59e35c12b676bde891791074e
Rossignol-h <ha.rossignol@laposte.net>
2023-01-10 20:44:20 +0100

    style(oc_lettings): add style to app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -9,3 +10,8 @@ urlpatterns = [
     path('profiles/', include(('profiles.urls', 'profiles'), namespace='profiles')),
     path('admin/', admin.site.urls),
 ]
+
+# ================================================================ STATIC/MEDIA
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
